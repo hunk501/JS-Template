c062fe40cc745f13bf1d1c10ef93aa58ed1a7870
Dennis Lira <lddsoftware501@gmail.com>
2019-06-29 02:32:40 +0800

    update in controller

--- controller.js
+++ controller.js
@@ -2,18 +2,22 @@ var RenderTemplate = (function() {
     
     var self = {};
 
-    // Private    
+    /**
+     | ----------------------------------
+     |  Private function
+     | ----------------------------------
+     */
     var generate = function(data) {
         return data.map(function(book){
             return `
-            <div id="div${ book.id }">
+            <div id="div${ book.id }" data-id="${ book.id }">
                 <p>ID: ${ book.id }</p>
                 <p>Title: ${ book.title }</p>
                 <p>Author: ${ book.author }</p>
                 <p id="book${book.id}"> 
                     Selected 
-                    <input type="radio" name="book${book.id}" value="1" ${ book.selected==1 ? 'checked':''}/> Yes &nbsp;
-                    <input type="radio" name="book${book.id}" value="0" ${ book.selected==0 ? 'checked':''}/> No
+                    <input onchange="RenderTemplate.clickRadio(this);" type="radio" name="book${book.id}" value="1" ${ book.selected==1 ? 'checked':''}/> Yes &nbsp;
+                    <input onchange="RenderTemplate.clickRadio(this);" type="radio" name="book${book.id}" value="0" ${ book.selected==0 ? 'checked':''}/> No
                     <button onclick="RenderTemplate.click(this);">Click</button>
                 </p>
             </div><hr>
@@ -21,18 +25,40 @@ var RenderTemplate = (function() {
         }).join(''); 
     }
 
-    // Public
+    /**
+     | ----------------------------------
+     |  Public function
+     | ----------------------------------
+     */
     self.render = function(domID, dataObject) {        
         document.getElementById(domID).innerHTML = generate(dataObject);   
     }
-
+    
+    // Button Click 
     self.click = function(element) {
         var parent = element.parentNode;
         var mother = parent.parentElement.id
-        var attr = document.getElementById(mother);
+        // Div
+        var attr = document.getElementById(mother);        
         attr.setAttribute('class', 'active');
+        
         console.log(parent.id);
         console.log(mother);
+        console.log(attr.getElementsByTagName('input'));
+
+        var radio = attr.getElementsByTagName('input');        
+        for(var i=0; i < radio.length; i++) {
+            console.log('value: '+ radio[i].value + ' checked: '+ radio[i].checked );
+        }
+    }
+
+    // Radio onchange
+    self.clickRadio = function(element) {
+        var current_element = element.parentNode;
+        var mother = current_element.parentNode;
+        console.log(element.value);
+        console.log(mother.id);
+        console.log(mother.getAttribute('data-id'));
     }
     
     return self;
